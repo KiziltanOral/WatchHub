4efc4910a96bf4074ea8fd3bdafc63cb6c9c83dc
Kiziltan Cihan Oral <kiziltancihanoral@gmail.com>
2023-09-17 16:28:02 +0300

    Entities, Configs, PostgreSQL connection

---
+++ README.md
@@ -0,0 +1,30 @@
+# WatchHub
+A Sample N-Layered .NET Core Project Demonstrating Clean Architecture and the Generic Repository Pattern
+
+## Packages
+
+### Application Core
+
+```
+Install-Package Ardalis.Specification
+```
+
+### Infrastructure
+```
+Install-Package Microsoft.EntityFrameworkCore
+Install-Package Npgsql.EntityFrameworkCore.PostgreSQL
+Install-Package Microsoft.EntityFrameworkCore.Tools
+Install-Package Microsoft.EntityFrameworkCore.Design
+Install-Package Microsoft.AspNetCore.Identity.EntityFrameworkCore
+Install-Package Ardalis.Specification.EntityFrameworkCore
+```
+
+## Migrations
+Before running the following commands, make sure that Web is set as startup project. Run the following commands on the project "Infrastructure".
+
+### Infrastructure
+```
+Add-Migration InitialCreate -Context WatchHubContext -Outputdir Data/Migrations
+Update-Database -Context WatchHubContext
+```
+
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,3 +1,4 @@
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Web.Data;
@@ -11,9 +12,12 @@ namespace Web
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<WatchHubContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("WatchHubContext")));
+
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
