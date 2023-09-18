4d4183d24c3f9aa7e569fa5fbfe30d9c890d2732
Kiziltan Cihan Oral <kiziltancihanoral@gmail.com>
2023-09-19 00:03:49 +0300

    pagination

--- src/Web/Models/HomeViewModel.cs
+++ src/Web/Models/HomeViewModel.cs
@@ -11,5 +11,6 @@ namespace Web.Models
 
         public int? CategoryId { get; set; }
         public int? BrandId { get; set; }
+        public PaginationInfoViewModel PaginationInfo { get; set; } = null!;
     }
 }
